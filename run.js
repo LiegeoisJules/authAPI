4a89c96e7404db43cf20837926d28cd8d822ed9d
Jules LIEGEOIS <julesliegeois@gmail.com>
2019-09-08 02:56:06 +0200

    initail commit

---
+++ run.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+//Import Routes
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+//Connect to DB
+mongoose.connect(process.env.DB_CONNECT,
+{ useNewUrlParser: true },
+() => {
+    console.log('Connected to DB');
+});
+
+//Middleware
+app.use(express.json());
+
+
+//Route Middleware
+app.use('/api/user', authRoute);
+
+app.listen(8080, () => console.log("Server start"));
